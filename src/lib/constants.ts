b694060fc3a3f93af46bbd701db3989428469ece
wkalidev <wkalidev@gmail.com>
2026-03-23 05:28:10 +0100

    feat: add shared constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,15 @@
+export const CONTRACT_ADDRESS = 'SP936YWJPST8GB8FFRCN7CC6P2YR5K6NNBAARQ96'
+export const CONTRACTS = {
+  token:       `${CONTRACT_ADDRESS}.b2s-token`,
+  governance:  `${CONTRACT_ADDRESS}.b2s-governance`,
+  liquidity:   `${CONTRACT_ADDRESS}.b2s-liquidity-pool-v5`,
+  rewards:     `${CONTRACT_ADDRESS}.b2s-rewards-distributor-v3`,
+  badges:      `${CONTRACT_ADDRESS}.b2s-badges`,
+  marketplace: `${CONTRACT_ADDRESS}.b2s-marketplace`,
+  staking:     `${CONTRACT_ADDRESS}.b2s-staking-vault-v2`,
+  feeRouter:   `${CONTRACT_ADDRESS}.b2s-fee-router`,
+}
+export const HIRO_API = 'https://api.mainnet.hiro.so'
+export const APP_URL  = 'https://base2stacks-tracker.vercel.app'
+export const DAILY_REWARD = 5
+export const BASE_APY = 12.5
