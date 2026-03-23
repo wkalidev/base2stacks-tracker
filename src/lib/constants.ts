447b8363f5c37a02bdb95ac4e2d1386331bea261
wkalidev <wkalidev@gmail.com>
2026-03-23 11:55:09 +0100

    fix: add HIRO_API export to constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,14 +1,15 @@
-export const APP_NAME    = 'Base2Stacks Bridge Tracker'
-export const APP_URL     = 'https://base2stacks-tracker.vercel.app'
-export const APP_TWITTER = '@willycodexwar'
-export const APP_GITHUB  = 'https://github.com/wkalidev/base2stacks-tracker'
+export const APP_NAME      = 'Base2Stacks Bridge Tracker'
+export const APP_URL       = 'https://base2stacks-tracker.vercel.app'
+export const APP_TWITTER   = '@willycodexwar'
+export const APP_GITHUB    = 'https://github.com/wkalidev/base2stacks-tracker'
 export const APP_FARCASTER = 'https://warpcast.com/willywarrior'
 
 export const CONTRACT_ADDRESS = 'SP936YWJPST8GB8FFRCN7CC6P2YR5K6NNBAARQ96'
+export const HIRO_API         = 'https://api.mainnet.hiro.so'
 
-export const DAILY_REWARD  = 5
-export const BASE_APY      = 12.5
-export const MAX_APY       = 37.5
-export const PLATFORM_FEE  = 2.5
-export const SWAP_FEE      = 0.25
-export const DECIMALS      = 6
+export const DAILY_REWARD = 5
+export const BASE_APY     = 12.5
+export const MAX_APY      = 37.5
+export const PLATFORM_FEE = 2.5
+export const SWAP_FEE     = 0.25
+export const DECIMALS     = 6
