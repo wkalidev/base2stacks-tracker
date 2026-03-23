fe56839422ce63dfd0330254c5c3908ae4d1c22e
wkalidev <wkalidev@gmail.com>
2026-03-23 11:42:40 +0100

    feat: add network configuration

---
+++ src/lib/network.ts
@@ -0,0 +1,22 @@
+export const NETWORKS = {
+  mainnet: {
+    name:    'Stacks Mainnet',
+    api:     'https://api.mainnet.hiro.so',
+    explorer:'https://explorer.hiro.so',
+  },
+  testnet: {
+    name:    'Stacks Testnet',
+    api:     'https://api.testnet.hiro.so',
+    explorer:'https://explorer.hiro.so/?chain=testnet',
+  },
+}
+
+export type NetworkName = keyof typeof NETWORKS
+
+export function getNetwork(name: NetworkName = 'mainnet') {
+  return NETWORKS[name]
+}
+
+export function getExplorerTxUrl(txId: string): string {
+  return `${NETWORKS.mainnet.explorer}/txid/${txId}?chain=mainnet`
+}
